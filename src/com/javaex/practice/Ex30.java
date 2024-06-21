6da4535068902a75aa8f42ada7b1589271bf3593
aripos1 <ihsjjang@gmail.com>
2024-06-21 16:41:40 +0900

    2024-06-21 최종

---
+++ src/com/javaex/practice/Ex30.java
@@ -0,0 +1,22 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex30 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("마일을 입력하세요: ");
+		
+		double m = sc.nextDouble();
+		
+		final double km = 1.609;
+		
+		System.out.println(m+"마일은 "+m*km+"킬로미터 입니다.");
+		
+		
+	}
+
+}
